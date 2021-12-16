8ce0982bae3677acd0c85bb10748310b75b8bdae
mikebutler12 <mikebutler12@users.noreply.github.com>
2021-12-16 16:12:34 +0000

    playing with pip packages and color

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -7,7 +7,7 @@ print(netifaces.interfaces())
 for i in netifaces.interfaces():
     print('\n****** details of interface - ' + i + ' ******')
     try:
-        print('MAC: ', end='') # This print statement will always print MAC without an end of line
+        print('MAC: ') # This print statement will always print MAC without an end of line
         print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
         print('IP: ', end='')  # This print statement will always print IP without an end of line
         print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
